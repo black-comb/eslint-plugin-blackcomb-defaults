208a4fbf6f87d725dbd166b7a238be2d2b9c4ae7
Marko Martin <black-comb@web.de>
2024-05-19 20:18:48 +0200

    Migrated to flat config (intermediate state).

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+import eslintPlugin from 'eslint-plugin-eslint-plugin';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+import javascript from './lib/configs/javascript.js';
+
+export default tseslint.config(
+  ...javascript,
+  eslintPlugin.configs['flat/recommended'],
+  {
+    name: 'blackcomb-defaults project base',
+    languageOptions: {
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'quote-props': ['error', 'consistent-as-needed'],
+      'quotes': ['error', 'single']
+    },
+  },
+  {
+    name: 'blackcomb-defaults project tests',
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.mocha
+      }
+    }
+  }
+);
